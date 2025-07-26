1683af7141e9a7d6de647ea7a100d78d943e7038
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-26 10:31:45 +0200

    Aula 156. Herança #02

--- 05-OrientacaoAObjetos/src/oo/heranca/Jogo.java
+++ 05-OrientacaoAObjetos/src/oo/heranca/Jogo.java
@@ -3,27 +3,30 @@ package oo.heranca;
 public class Jogo {
     public static void main(String[] args) {
 
-        Monstro j1 = new Monstro();
-        j1.x = 10; //Eixo X: Jogador1 inicia na posição pré-estabelecida
-        j1.y = 10; //Eixo Y: Jogador1 inicia na posição pré-estabelecida
+        Monstro monstro = new Monstro();
+        monstro.x = 10; //Eixo X: Posição pré-estabelecida
+        monstro.y = 10; //Eixo Y: Posição pré-estabelecida
 
-        Heroi j2 = new Heroi();
-        j2.x = 10; //Eixo X: Jogador2 inicia na posição pré-estabelecida
-        j2.y = 11; //Eixo Y: Jogador2 inicia na posição pré-estabelecida
+        Heroi heroi = new Heroi();
+        heroi.x = 10; //Eixo X: Posição pré-estabelecida
+        heroi.y = 11; //Eixo Y: Posição pré-estabelecida
 
-        System.out.println("Life Monstro: " + j1.life);
-        System.out.println("Life Heroi: " + j2.life);
+        System.out.println("Life Monstro: " + monstro.life);
+        System.out.println("Life Heroi: " + heroi.life);
 
-        j1.atacar(j2);
-        System.out.println("\nMonstro ataca Heroi!");
-        System.out.println("Life Monstro: " + j1.life);
-        System.out.println("Life Heroi: " + j2.life);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-        j2.atacar(j1);
-        System.out.println("\nContra ataque do Heroi!");
-        System.out.println("Life Monstro: " + j1.life);
-        System.out.println("Life Heroi: " + j2.life);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
+        monstro.andar(Direcao.NORTE);
+        System.out.println("\nMonstro fugiu para o Norte!");
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
+        System.out.println("\nApós Batalha:");
+        System.out.println("Life Monstro: " + monstro.life);
+        System.out.println("Life Heroi: " + heroi.life);
     }
 }
