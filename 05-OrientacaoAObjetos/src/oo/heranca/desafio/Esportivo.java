ff2aa71b3147177fa55a0176e4511142137fabcc
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-28 09:25:04 +0200

    Aula 169. Interface #01

---
+++ 05-OrientacaoAObjetos/src/oo/heranca/desafio/Esportivo.java
@@ -0,0 +1,6 @@
+package oo.heranca.desafio;
+
+public interface Esportivo {
+    public abstract void ligarTurbo(); //public abstract por padrão em uma interface
+    public abstract void desligarTurbo(); //public abstract por padrão em uma interface
+}
--- 05-OrientacaoAObjetos/src/oo/heranca/teste/Autodromo.java
+++ 05-OrientacaoAObjetos/src/oo/heranca/teste/Autodromo.java
@@ -6,10 +6,14 @@ import oo.heranca.desafio.Fusca;
 
 public class Autodromo {
     public static void main(String[] args) {
-        Carro ferrari = new Ferrari(400);
+        Ferrari ferrari = new Ferrari(400);
         Carro fusca = new Fusca();
 
         System.out.println("\nFerrari:");
+        ferrari.ligarTurbo();
+        ferrari.ligarAr();
+        ferrari.desligarAr();
+
         ferrari.acelerar();
         ferrari.frear();
         System.out.println(ferrari);
@@ -28,6 +32,19 @@ public class Autodromo {
         System.out.println(ferrari);
         ferrari.frear();
         System.out.println(ferrari);
+        ferrari.desligarTurbo();
+        ferrari.frear();
+        ferrari.frear();
+        ferrari.frear();
+        System.out.println(ferrari);
+        ferrari.frear();
+        ferrari.frear();
+        ferrari.frear();
+        System.out.println(ferrari);
+        ferrari.frear();
+        ferrari.frear();
+        System.out.println(ferrari);
+
 
         System.out.println("\nFusca:");
         fusca.acelerar();
