9a0bfcb2709db81653eb7ddced207219197fd097
pitercoding <piterg.bio@gmail.com>
2025-09-19 17:54:18 +0200

    feat (aula): 180. Foreach Funcional #02

--- 06-Lambdas/src/lambdas/Foreach.java
+++ 06-Lambdas/src/lambdas/Foreach.java
@@ -14,11 +14,21 @@ public class Foreach {
         }
 
         System.out.println("\nLambda 01:");
-        aprovados.forEach((nome) -> {
-            System.out.println(nome + "!!!");
-        });
+        aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 
-        System.out.println("\nMethod Reference:");
+        System.out.println("\nMethod Reference 01:");
         aprovados.forEach(System.out::println);
+
+        // Usando método meuImprimir
+        System.out.println("\nLambda 02:");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+        System.out.println("\nMethod Reference 02:");
+        aprovados.forEach(Foreach::meuImprimir);
+    }
+
+    //Método personalizado para usar lambda e/ou method reference
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! Meu nome é " + nome);
     }
 }
