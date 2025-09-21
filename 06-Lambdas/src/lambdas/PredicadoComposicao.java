e5b193a8cce9d393ae94481fefb28d896d1dca2a
pitercoding <piterg.bio@gmail.com>
2025-09-21 13:33:19 +0200

    feat(aula): 185. Predicate #02

---
+++ 06-Lambdas/src/lambdas/PredicadoComposicao.java
@@ -0,0 +1,18 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+    public static void main(String[] args) {
+        Predicate<Integer> isPar = num -> num % 2 == 0;
+        Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+
+        System.out.println(isPar.test(123)); // false
+        System.out.println(isPar.test(122)); // true
+
+        System.out.println(isPar.and(isTresDigitos).test(122));
+        System.out.println(isPar.and(isTresDigitos).negate().test(123));
+        System.out.println(isPar.or(isTresDigitos).test(123));
+
+    }
+}
