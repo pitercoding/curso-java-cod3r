8dced10749432724374ce0365c4e621f7d4c493d
pitercoding <piterg.bio@gmail.com>
2025-10-16 17:46:31 +0200

    feat(aula): 344. DAO

---
+++ 14-JPA/src/infra/DAO.java
@@ -0,0 +1,71 @@
+package infra;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class DAO<E> {
+
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> classe;
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory("14-JPA");
+        } catch (Exception e) {
+            // logar -> log
+        }
+    }
+
+    public DAO() {
+        this(null);
+    }
+
+    public DAO(Class<E> classe) {
+        this.classe = classe;
+        em = emf.createEntityManager();
+    }
+
+    // Métodos //
+    public DAO<E> abrirTransacao() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public DAO<E> fecharTransacao() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public DAO<E> incluir(E entidade) {
+        em.persist(entidade);
+        return this;
+    }
+
+    public DAO<E> incluirAtomico(E entidade) {
+        return this.abrirTransacao().incluir(entidade).fecharTransacao();
+    }
+
+    public List<E> obterTodos() {
+        return this.obterTodos(10, 0);
+    }
+
+    public List<E> obterTodos(int qtde, int deslocamento) {
+        if (classe == null) {
+            throw  new UnsupportedOperationException("Classe nula.");
+        }
+
+        String jpql = "select e from " + classe.getName() + " e";
+        TypedQuery<E> query = em.createQuery(jpql, classe);
+        query.setMaxResults(qtde);
+        query.setFirstResult(deslocamento);
+        return query.getResultList();
+    }
+
+    public void fecharDAO() {
+        em.close();
+    }
+}
