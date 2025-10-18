ec1e3269c0537ef5ca21a2bc15467547bb0957cc
pitercoding <piterg.bio@gmail.com>
2025-10-18 17:16:43 +0200

    feat(aula): 349. Um Pra Um #02

--- 14-JPA/src/modelo/umpraum/Cliente.java
+++ 14-JPA/src/modelo/umpraum/Cliente.java
@@ -12,7 +12,7 @@ public class Cliente {
 
     private String nome;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "assento_id", unique = true)
     private Assento assento;
 
