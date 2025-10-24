92c3aaa39b7836d2ac9cdfa385ad888c4ad6813a
pitercoding <piterg.bio@gmail.com>
2025-10-24 14:25:34 +0200

    feat(aula): 427. Passando Parâmetros para Web Service #02

--- 20-SpringBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ 20-SpringBoot/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -18,4 +18,10 @@ public class ClienteController {
     public Cliente obterClientePorId1(@PathVariable int id){
         return new Cliente(id, "Maria", "987.654.321-00");
     }
+
+    @GetMapping()
+    public Cliente obterClientePorId2(
+            @RequestParam(name = "id", defaultValue = "1") int id){
+        return new Cliente(id, "João Augusto", "111.222.333-44");
+    }
 }
